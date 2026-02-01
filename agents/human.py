8607e7553a38ded9d87063893a28cc537aca295f
Kemonoo <michal.petruf@gmail.com>
2026-02-01 14:32:41 +0100

    phantom state game dependend functions

---
+++ agents/human.py
@@ -0,0 +1,4 @@
+
+class HumanPlayer():
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+
+from agents.human import HumanPlayer
+from phantom_ttt_state import PhantomTTTState
+
+
+def play_game(p1, p2):
+    pass
